727d9a3a77ed31cb97433e98912f45f7d5d5bb84
happ-in <gpqls304@naver.com>
2021-12-19 22:10:06 +0900

    Feat: Feat Login page

--- front/WakeUp/src/App.vue
+++ front/WakeUp/src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <Login />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Login from './components/Login.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Login
   }
 }
 </script>
---
+++ front/WakeUp/src/components/Login.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      <div>
+          Wake Up
+      </div>
+
+      <div>
+          <img src="../assets/kakao_login.png" alt="error">          
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
